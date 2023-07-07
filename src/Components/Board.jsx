0ed307431679597e7069d53dce3f93a35ffbd665
shubham kumar <kumarshubham417@gmail.com>
2023-07-07 19:54:35 +0530

    row structure defined in board component

--- src/Components/Board.jsx
+++ src/Components/Board.jsx
@@ -1,19 +1,20 @@
 import React from "react";
+import styles from "../Styles/Board.module.css";
 
 function Board() {
   return (
-    <div className={StyleSheet.container}>
-      <div className={StyleSheet.row_container}>
+    <div className={styles.container}>
+      <div className={styles.row_container}>
         <div></div>
         <div></div>
         <div></div>
       </div>
-      <div className={StyleSheet.row_container}>
+      <div className={styles.row_container}>
         <div></div>
         <div></div>
         <div></div>
       </div>
-      <div className={StyleSheet.row_container}>
+      <div className={styles.row_container}>
         <div></div>
         <div></div>
         <div></div>
