72fbb766c19bac86d959eb361b2c017f17f6f13f
shubham kumar <kumarshubham417@gmail.com>
2023-07-07 20:31:24 +0530

    board component designed

--- src/Components/Board.jsx
+++ src/Components/Board.jsx
@@ -1,24 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../Styles/Board.module.css";
 import Card from "./Card";
 
+const initialState = [null, null, null, null, null, null, null, null, null];
+
 function Board() {
+  const [state, setState] = useState(initialState);
   return (
     <div className={styles.container}>
       <div className={styles.row_container}>
-        <div><Card /></div>
-        <div><Card /></div>
-        <div><Card /></div>
+        <div>
+          <Card value={state[0]} />
+        </div>
+        <div>
+          <Card value={state[1]} />
+        </div>
+        <div>
+          <Card value={state[2]} />
+        </div>
       </div>
       <div className={styles.row_container}>
-        <div><Card /></div>
-        <div><Card /></div>
-        <div><Card /></div>
+        <div>
+          <Card value={state[3]} />
+        </div>
+        <div>
+          <Card value={state[4]} />
+        </div>
+        <div>
+          <Card value={state[5]} />
+        </div>
       </div>
       <div className={styles.row_container}>
-        <div><Card /></div>
-        <div><Card /></div>
-        <div><Card /></div>
+        <div>
+          <Card value={state[6]} />
+        </div>
+        <div>
+          <Card value={state[7]} />
+        </div>
+        <div>
+          <Card value={state[8]} />
+        </div>
       </div>
     </div>
   );
