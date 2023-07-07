3859dd4e24f65945f1950918f906a66cec484821
shubham kumar <kumarshubham417@gmail.com>
2023-07-07 23:12:23 +0530

    live active player turn showing

--- src/Components/Board.jsx
+++ src/Components/Board.jsx
@@ -24,31 +24,33 @@ function Board() {
 
   return (
     <>
-    <span>Player "O"</span>
-    <span>Player "X"</span>
-    <div className={styles.container}>
-      <div className={styles.row_container}>
-        <Card value={state[0]} index={0} handleClick={handleClick} />
-
-        <Card value={state[1]} index={1} handleClick={handleClick} />
-
-        <Card value={state[2]} index={2} handleClick={handleClick} />
+      <div className={styles.active_player}>
+        <span className={isxTurn?styles.default:styles.active}>Turn- O</span>
+        <span className={isxTurn?styles.active:styles.default}>Turn- X</span>
       </div>
-      <div className={styles.row_container}>
-        <Card value={state[3]} index={3} handleClick={handleClick} />
+      <div className={styles.container}>
+        <div className={styles.row_container}>
+          <Card value={state[0]} index={0} handleClick={handleClick} />
 
-        <Card value={state[4]} index={4} handleClick={handleClick} />
+          <Card value={state[1]} index={1} handleClick={handleClick} />
 
-        <Card value={state[5]} index={5} handleClick={handleClick} />
+          <Card value={state[2]} index={2} handleClick={handleClick} />
+        </div>
+        <div className={styles.row_container}>
+          <Card value={state[3]} index={3} handleClick={handleClick} />
+
+          <Card value={state[4]} index={4} handleClick={handleClick} />
+
+          <Card value={state[5]} index={5} handleClick={handleClick} />
+        </div>
+        <div className={styles.row_container}>
+          <Card value={state[6]} index={6} handleClick={handleClick} />
+
+          <Card value={state[7]} index={7} handleClick={handleClick} />
+
+          <Card value={state[8]} index={8} handleClick={handleClick} />
+        </div>
       </div>
-      <div className={styles.row_container}>
-        <Card value={state[6]} index={6} handleClick={handleClick} />
-
-        <Card value={state[7]} index={7} handleClick={handleClick} />
-
-        <Card value={state[8]} index={8} handleClick={handleClick} />
-      </div>
-    </div>
     </>
   );
 }
