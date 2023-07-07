bb64aa1ce91be93a987f6e91059cf456d44dcac1
shubham kumar <kumarshubham417@gmail.com>
2023-07-07 19:57:14 +0530

    card component in board component

--- src/Components/Board.jsx
+++ src/Components/Board.jsx
@@ -1,23 +1,24 @@
 import React from "react";
 import styles from "../Styles/Board.module.css";
+import Card from "./Card";
 
 function Board() {
   return (
     <div className={styles.container}>
       <div className={styles.row_container}>
-        <div></div>
-        <div></div>
-        <div></div>
+        <div><Card /></div>
+        <div><Card /></div>
+        <div><Card /></div>
       </div>
       <div className={styles.row_container}>
-        <div></div>
-        <div></div>
-        <div></div>
+        <div><Card /></div>
+        <div><Card /></div>
+        <div><Card /></div>
       </div>
       <div className={styles.row_container}>
-        <div></div>
-        <div></div>
-        <div></div>
+        <div><Card /></div>
+        <div><Card /></div>
+        <div><Card /></div>
       </div>
     </div>
   );
