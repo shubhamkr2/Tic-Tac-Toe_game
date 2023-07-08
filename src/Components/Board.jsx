0c61ed558514b5b6d45c52022b6eff19dbc439e1
shubham kumar <kumarshubham417@gmail.com>
2023-07-08 17:46:34 +0530

    winner board added

--- src/Components/Board.jsx
+++ src/Components/Board.jsx
@@ -22,39 +22,69 @@ function Board() {
     return;
   }
 
+  function checkWinner() {
+    const winningCombinations = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8], // Horizontal
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8], // Vertical
+      [0, 4, 8],
+      [2, 4, 6], // Diagonal
+    ];
+
+    for (const combination of winningCombinations) {
+      const [a, b, c] = combination;
+      if (state[a] !== null && state[a] === state[b] && state[a] === state[c]) {
+        // We have a winner
+        return state[a];
+      }
+    }
+    // No winner found
+    return null;
+  }
+  let winner = checkWinner();
+  console.log(winner);
   return (
     <>
-      <div className={styles.active_player}>
-        <span className={isxTurn ? styles.default : styles.active}>
-          (A) Turn- O
-        </span>
-        <span className={isxTurn ? styles.active : styles.default}>
-          (B) Turn- X
-        </span>
-      </div>
-      <div className={styles.container}>
-        <div className={styles.row_container}>
-          <Card value={state[0]} index={0} handleClick={handleClick} />
+      {winner ? (
+        <div className={styles.winner}>Winner {winner}</div>
+      ) : (
+        <>
+          <div className={styles.active_player}>
+            <span className={isxTurn ? styles.default : styles.active}>
+              (A) Turn- O
+            </span>
+            <span className={isxTurn ? styles.active : styles.default}>
+              (B) Turn- X
+            </span>
+          </div>
+          <div className={styles.container}>
+            <div className={styles.row_container}>
+              <Card value={state[0]} index={0} handleClick={handleClick} />
 
-          <Card value={state[1]} index={1} handleClick={handleClick} />
+              <Card value={state[1]} index={1} handleClick={handleClick} />
 
-          <Card value={state[2]} index={2} handleClick={handleClick} />
-        </div>
-        <div className={styles.row_container}>
-          <Card value={state[3]} index={3} handleClick={handleClick} />
+              <Card value={state[2]} index={2} handleClick={handleClick} />
+            </div>
+            <div className={styles.row_container}>
+              <Card value={state[3]} index={3} handleClick={handleClick} />
 
-          <Card value={state[4]} index={4} handleClick={handleClick} />
+              <Card value={state[4]} index={4} handleClick={handleClick} />
 
-          <Card value={state[5]} index={5} handleClick={handleClick} />
-        </div>
-        <div className={styles.row_container}>
-          <Card value={state[6]} index={6} handleClick={handleClick} />
+              <Card value={state[5]} index={5} handleClick={handleClick} />
+            </div>
+            <div className={styles.row_container}>
+              <Card value={state[6]} index={6} handleClick={handleClick} />
 
-          <Card value={state[7]} index={7} handleClick={handleClick} />
+              <Card value={state[7]} index={7} handleClick={handleClick} />
 
-          <Card value={state[8]} index={8} handleClick={handleClick} />
-        </div>
-      </div>
+              <Card value={state[8]} index={8} handleClick={handleClick} />
+            </div>
+          </div>
+        </>
+      )}
       <button className={styles.reset} onClick={() => setState(initialState)}>
         Reset
       </button>
